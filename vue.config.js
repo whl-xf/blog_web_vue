774960ff1694f5d58b64627903a851314d727fe8
hailin <wbhailin_wang@kingdee.com>
2022-08-06 00:29:50 +0800

    blog web

---
+++ vue.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  transpileDependencies: ["vuetify"],
+  devServer: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:8080",
+        changeOrigin: true,
+        pathRewrite: {
+          "^/api": ""
+        }
+      }
+    }
+    // ,
+    // disableHostCheck: true
+  },
+  productionSourceMap: false,
+  css: {
+    extract: true,
+    sourceMap: false
+  },
+  lintOnSave: false
+};
